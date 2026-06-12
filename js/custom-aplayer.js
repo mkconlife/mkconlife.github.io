cabc15142e607d4e768772581f72aee885702c8a
mkconlife <3297602270@qq.com>
2026-06-12 17:37:03 +0800

    Site updated: 2026-06-12 17:37:01

---
+++ js/custom-aplayer.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var container = document.createElement('div');
+    container.id = 'aplayer';
+    container.style.cssText = 'position:fixed;bottom:0;left:0;z-index:999;width:100%;';
+    document.body.appendChild(container);
+
+    new APlayer({
+        container: container,
+        fixed: true,
+        audio: [{
+            name: 'Welt Joyce',
+            artist: 'HOYO-MIX',
+            url: '/music/welt-joyce.mp3',
+            cover: '/music/picture/welt-joyce.png'
+        }]
+    });
+});
